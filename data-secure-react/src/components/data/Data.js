4f0d746ad08b2c7becd95cb4b1e00aada3355f81
DylanLev <dylan.levy5@yahoo.com>
2023-03-20 21:10:10 +0200

    Data Component

---
+++ data-secure-react/src/components/data/Data.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DataStyles.css'
+
+const Data = () => {
+  return (
+    <div className='data'>
+        <div className="container">
+            <div className="content">
+                <h2><span>Data</span> Recovery</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae in totam dicta aliquam id laborum repudiandae mollitia beatae autem cupiditate reprehenderit quae quaerat doloribus eos dolorum ex ab, quasi amet.</p>
+                <button>Sign in</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Data
