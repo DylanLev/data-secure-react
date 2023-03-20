7189cd644de1d8aab8932ede8634c3cc3cb1ed62
DylanLev <dylan.levy5@yahoo.com>
2023-03-20 20:58:26 +0200

    Navbar onclick

--- data-secure-react/src/components/navbar/Navbar.js
+++ data-secure-react/src/components/navbar/Navbar.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import './NavbarStyles.css'
 import { SiDatabricks } from 'react-icons/si'
-import { FaBars} from 'react-icons/fa'
+import { FaBars, FaTimes} from 'react-icons/fa'
 const Navbar = () => {
+    const [nav,setNav] = useState(false);
+    const handleNav = () => setNav(!nav);
+
+
   return (
     <div className='navbar'>
-        <div className="conainer">
-            <div className="logo">
+        <div className='conainer'>
+            <div className='logo'>
                 <SiDatabricks className='icon'/>
                 <h1>Secured</h1>
             </div>
-            <ul className="nav-menu">
+            <ul className= {nav?'nav-menu active':'nav menu'}>
                 <li>Home</li>
                 <li>Recovery</li>
                 <li>Cloud</li>
                 <li>Contact</li>
                 <button>Sign in</button>
             </ul>
-            <div className="humburger">
-                <FaBars/>
+            <div className='humburger' onClick={ handleNav }>
+                {!nav ? (<FaBars className='icon'/>):(<FaTimes className='icon'/>)}
+                
             </div>
         </div>
     </div>
