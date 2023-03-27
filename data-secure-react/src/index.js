89ea856124c732d87e71550b592c1f98164523e6
DylanLev <dylan.levy5@yahoo.com>
2023-03-27 14:00:46 +0300

    Cloud, Contact and Recovery Pages

--- data-secure-react/src/index.js
+++ data-secure-react/src/index.js
@@ -2,20 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Recovery from './routes/Recovery'
-import CloudPage from './routes/CloudPage'
-import ContactPage from './routes/ContactPage'
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import RecoveryPage from './routes/RecoveryPage'
+// import CloudPage from './routes/CloudPage'
+// import ContactPage from './routes/ContactPage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}/>
-      <Route path='/recovery' element={<Recovery />}/>
-      <Route path='/cloud' element={<CloudPage />}/>
-      <Route path='/contact' element={<ContactPage />}/>
-    </Routes>
-  </BrowserRouter>
+      <App/>
 );
 
